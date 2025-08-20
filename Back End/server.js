468a28c940bef4cadb6089a90044a28bc9add502
ahmadkhatib02 <ahmad.alkhatib02@lau.edu>
2025-08-20 17:11:58 +0300

    retrying

--- Back End/server.js
+++ Back End/server.js
@@ -1,6 +1,6 @@
 const app = require("./app.js");
 const port = 3000;
 
-// app.listen(port, () =>
-//   console.log(`Server Started on http://127.0.0.1:${port}`)
-// );
+app.listen(port, () =>
+  console.log(`Server Started on http://127.0.0.1:${port}`)
+);
